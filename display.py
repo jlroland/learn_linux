0b510ecc3d359b7a05758b9cc93643ba5b0b7f2e
Jessica Roland <jlroland@user.noreply.github.com>
2022-04-27 15:41:04 -0700

    Refactored code to draw grid, starting to add text

--- display.py
+++ display.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python3
 
-import sys, pygame
-pygame.init()
+import sys
+import pygame as pg
+import questions as quest
+pg.init()
+
 
 width, height = 600, 600
+grid_size = 3
 black = 0, 0, 0
 white = 255, 255, 255
 
-screen = pygame.display.set_mode((width, height))
+screen = pg.display.set_mode((width, height))
 
 
-player = pygame.image.load("./learn_linux/weird_donkey.png")
+player = pg.image.load("./weird_donkey.png")
 player_rect = player.get_rect()
 player_pos = [250, 250]
 dist = 50
 
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
+    for event in pg.event.get():
+        if event.type == pg.QUIT: 
+            sys.exit()
+        if event.type == pg.KEYDOWN or event.type == pg.KEYUP:
+            if event.key == pg.K_UP:
                 player_pos[1] -= dist
-            if event.key == pygame.K_DOWN:
+            if event.key == pg.K_DOWN:
                 player_pos[1] += dist
-            if event.key == pygame.K_LEFT:
+            if event.key == pg.K_LEFT:
                 player_pos[0] -= dist
-            if event.key == pygame.K_RIGHT:
+            if event.key == pg.K_RIGHT:
                 player_pos[0] += dist
 
 
     screen.fill(black)
-    for i in range(5):
-        for j in range(5):
-            pygame.draw.line(screen, white, (0, i*width/5), (width, i*width/5))
-            pygame.draw.line(screen, white, (j*height/5, 0), (j*height/5, height))
+    font = pg.font.Font(None, 28)
+    text = font.render("Question here", True, white)
+    textpos = text.get_rect(centerx=screen.get_width() / 2, y=10)
+    screen.blit(text, textpos)
+    for i in range(grid_size):
+        for j in range(grid_size):
+            grid_box = pg.Rect(i*width/grid_size, j*width/grid_size, width/grid_size, height/grid_size)
+            pg.draw.rect(screen, white, grid_box, 1)
+            font = pg.font.Font(None, 28)
+            text = font.render("answer here", True, white)
+            #text_box = pg.Rect(i*width*0.5/grid_size, j*width*0.5/grid_size, width/grid_size, height/grid_size)
+            #textpos = text.get_rect(centerx=grid_box.get_width() / 2, y=10)
+            screen.blit(text, grid_box)
+   
+    # font = pg.font.Font(None, 64)
+    # text = font.render("Pummel The Chimp, And Win $$$", True, white)
+    # textpos = text.get_rect(centerx=screen.get_width() / 2, y=10)
+    # screen.blit(text, textpos)
     screen.blit(player, player_pos)
-    pygame.display.flip()
+    pg.display.flip()
 
+
+
+ # for i in range(grid_size):  
+    #     pg.draw.line(screen, white, (0, i*width/grid_size), (width, i*width/grid_size))       
+    # for j in range(grid_size):
+    #     pg.draw.line(screen, white, (j*height/grid_size, 0), (j*height/grid_size, height))
--- questions.py
+++ questions.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 
+question1 = "You see a file named devices.txt. Which commands are valid for this file type?"
+
+correct_answers1 = {
+    '1': 'mv',
+    '2': 'cp',
+    '3': 'rm',
+    '4': 'chmod',
+    '5': 'touch'
+}
+
+wrong_answers1 = {
+    '1': 'rmdir',
+    '2': 'cd',
+    '3': 'pwd'
+}
