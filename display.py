ca6e406d1305571c27a578ae4b630ad3c3a1b0de
Jessica Roland <jlroland@user.noreply.github.com>
2022-04-29 08:26:22 -0700

    Add ending to level/game, update graphic, limit user movement

--- display.py
+++ display.py
@@ -2,25 +2,36 @@
 
 import sys
 import pygame as pg
-#import numpy as np
-import questions as quest
+import numpy as np
+import questions as qs
 pg.init()
 
 width, height = 600, 600
 grid_size = 3
 black = 0, 0, 0
 white = 255, 255, 255
+red = 255, 0, 0
+green = 0, 255, 0
 
 screen = pg.display.set_mode((width, height))
 
-player = pg.image.load("./weird_donkey.png")
+player = pg.image.load("./cute-pink-toy-pxhere.jpg")
 player_rect = player.get_rect()
 player_pos = [width/2, height/2]
 dist = width/grid_size
 
-answers = quest.answer_shuffle(quest.level1, grid_size)
+answers = qs.answer_shuffle(qs.level1, grid_size)
+score = 0
+penalty = 0
+penalty_switch = 0
+win_switch = 0
 
 def select_answer():
+    global score
+    global penalty
+    global penalty_switch
+    global win_switch
+
     i, j = 0, 0
     if player_pos[0] == 300:
         j = 1
@@ -30,34 +41,41 @@ def select_answer():
         i = 1
     elif player_pos[1] == 500:
         i = 2
+    answers[1,1] = 'End'
     
     chosen_answer = answers[j,i]   # text loop went wonky?
-    if chosen_answer in quest.level1["correct_answers"]:
-        print("Correct!")
-    elif chosen_answer == 'Start':
-        print("What are you doing?")
-    else:
-        print("Wrong!")
+    if chosen_answer in qs.level1["correct_answers"]:
+        score += 10
+        answers[j,i] = ''
+    elif chosen_answer in qs.level1["wrong_answers"]:
+        answers[j,i] = ''
+        penalty += 1
+    elif chosen_answer == 'End':
+        check_answers = set(answers.flatten()).intersection(set(qs.level1["correct_answers"]))
+        if check_answers:
+            penalty_switch =1
+        else:
+            win_switch =1
 
 while 1:
     for event in pg.event.get():
         if event.type == pg.QUIT: 
             sys.exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_UP:
+            if event.key == pg.K_UP and player_pos[1] > 200:
                 player_pos[1] -= dist
-            if event.key == pg.K_DOWN:
+            if event.key == pg.K_DOWN and player_pos[1] < 500:
                 player_pos[1] += dist
-            if event.key == pg.K_LEFT:
+            if event.key == pg.K_LEFT and player_pos[0] > 200:
                 player_pos[0] -= dist
-            if event.key == pg.K_RIGHT:
+            if event.key == pg.K_RIGHT and player_pos[0] < 500:
                 player_pos[0] += dist
             if event.key == pg.K_SPACE:
                 select_answer()
 
     screen.fill(black)
-    font = pg.font.Font(None, 22)
-    text = font.render(quest.level1['question'], True, white)
+    font = pg.font.Font(None, 24)
+    text = font.render(qs.level1['question'], True, white)
     textpos = text.get_rect(centerx=screen.get_width() / 2, y=10)
     screen.blit(text, textpos)
 
@@ -69,6 +87,22 @@ while 1:
             text = font.render(answers[i,j], True, white)
             screen.blit(text, grid_box.center)
 
+    if penalty > 1 or penalty_switch == 1:
+        end_font = pg.font.Font(None, 72)
+        end_text = end_font.render("GAME OVER", True, red)
+        end_textpos = end_text.get_rect(centerx=screen.get_width()/2, centery=screen.get_height()/2)
+        screen.blit(end_text, end_textpos)
+    
+    if win_switch == 1:
+        end_font = pg.font.Font(None, 72)
+        end_text = end_font.render("YOU'RE A WINNER!", True, green)
+        end_textpos = end_text.get_rect(centerx=screen.get_width()/2, centery=screen.get_height()/2)
+        screen.blit(end_text, end_textpos)
+
+
     screen.blit(player, player_pos)
     pg.display.flip()
+
+    if penalty > 1 or penalty_switch == 1 or win_switch:
+        pg.event.clear(eventtype=pg.KEYDOWN)  
 
